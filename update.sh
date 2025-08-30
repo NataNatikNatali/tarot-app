c089a5f1b8e76b8a7585526e3a0214414e386e1a
NataNatikNatali <navinatik@gmail.com>
2025-08-30 06:10:18 +0300

    Обновление проекта + workflow для APK

---
+++ update.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cd ~/tarot-app/tarot-app || exit
+
+echo "🔄 Проверяем GitHub origin..."
+git remote remove origin 2>/dev/null
+git remote add origin https://x-access-token:$GITHUB_TOKEN@github.com/natanatiknatali/tarot-app.git
+
+git remote -v
+
+echo "📦 Добавляем файлы..."
+git add .
+
+echo "📝 Делаем коммит..."
+git commit -m "Обновление проекта" || echo "⚠️ Нет новых изменений для коммита."
+
+echo "🚀 Отправляем проект на GitHub..."
+git push -u origin main
